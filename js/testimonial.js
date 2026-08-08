ad095632d03058f293d803b9156f66a0bf7e7af1
lamg2091 <luismejiam1991@gmail.com>
2026-08-08 07:28:04 -0500

    nuevos ajsutes

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,1 +1,17 @@
+const containerTestimonials = document.querySelector('.container-testimonials');
+const cardsTestimonials = document.querySelectorAll('.testimonial')
+const btnPrev = documnet.querySelector('.btn-prev')
+const btnNext = documnet.querySelector('.btn-next')
 
+
+let currentIndex = 0;
+const totalTestimonials = cardsTestimonials.length;
+let autoplayInterval;
+
+const updateCarrusel = () =>{
+    const offset = currentIndex * containerTestimonials.clientWidth;
+    containerTestimonials.scrollTo({
+        left: offset,
+        behavior: 'smooth'
+    })
+}
