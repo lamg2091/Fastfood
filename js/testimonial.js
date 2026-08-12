9567aa55bee1f31c8b0f45eb610035806c534a61
lamg2091 <luismejiam1991@gmail.com>
2026-08-12 15:50:27 -0500

    se agrego las nuevos avances

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,17 +1,41 @@
-const containerTestimonials = document.querySelector('.container-testimonials');
-const cardsTestimonials = document.querySelectorAll('.testimonial')
-const btnPrev = documnet.querySelector('.btn-prev')
-const btnNext = documnet.querySelector('.btn-next')
-
+const containerTestimonials = document.querySelector(".container-testimonials");
+const cardsTestimonials = document.querySelectorAll(".testimonial");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
 
 let currentIndex = 0;
 const totalTestimonials = cardsTestimonials.length;
 let autoplayInterval;
 
-const updateCarrusel = () =>{
-    const offset = currentIndex * containerTestimonials.clientWidth;
-    containerTestimonials.scrollTo({
-        left: offset,
-        behavior: 'smooth'
-    })
+const updateCarrusel = () => {
+  const offset = currentIndex * containerTestimonials.clientWidth;
+  containerTestimonials.scrollTo({
+    left: offset,
+    behavior: "smooth",
+  });
+};
+btnNext.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % totalTestimonials;
+  updateCarrusel();
+  resetautoplay()
+});
+btnPrev.addEventListener("click", () => {
+  currentIndex = currentIndex - 1 + (totalTestimonials % totalTestimonials);
+  updateCarrusel();
+  resetautoplay()
+});
+
+function autoplay() {
+  autoplayInterval = setInterval(() => {
+    currentIndex = currentIndex = (currentIndex + 1) % totalTestimonials;
+    updateCarrusel();
+  }, 5000);
+  
 }
+function resetautoplay(){
+    clearInterval(autoplayInterval)
+    autoplay()
+}
+
+autoplay()
+
